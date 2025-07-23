3406dc912ac9b0fb9ed0dc9882986c07c46c7a50
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-07-23 19:24:45 +0530

    Validation Check

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,25 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
 
 const resgisterUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: "COOL"
+    const {fullname, email, username, password} = req.body
+    console.log("Email: ",email);
+    console.log("Password: ",password);
+
+    if ([fullname, email, username, password].some((field)=>field?.trim()==="")) {
+    throw new ApiError(400,"All fields are MUST!")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username },{ email }]
     })
+
+    if (existedUser) {
+        throw new ApiError(409,"User Already Exists")
+    }
+
+    req.files?.avatar
 })
 
 export {resgisterUser}
